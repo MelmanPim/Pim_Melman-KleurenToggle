110d7b0df1e418899640c6eb9a54852160852c4a
MelmanPim <59539663+MelmanPim@users.noreply.github.com>
2021-07-20 10:07:54 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+// Open menu on click
+/* const colorNavigation = document.querySelector('#navigationContainer');
+
+const showMenu = () => {
+    colorNavigation.classList.add('show');
+}
+
+colorNavigation.addEventListener('onmouseenter', showMenu); */
+
+document.getElementById("navigationContainer").addEventListener("mouseenter", mouseEnter);
+document.getElementById("navigationContainer").addEventListener("mouseleave", mouseLeave);
+
+function mouseEnter() {
+    document.getElementById("navigation").style.left = "0.5em";
+}
+
+function mouseLeave() {
+    document.getElementById("navigation").style.left = "-30em";
+}
+
+// Verander achtergrond kleur
+
 const background = document.querySelector('.background');
 
 const makeRed = document.querySelector('#red');
@@ -6,29 +28,29 @@ const makePurple = document.querySelector('#purple');
 const makeGreen = document.querySelector('#green');
 
 const redBackground = () => {
-    background.classList.add('red');
-    background.classList.remove('orange', 'purple', 'green');
+    document.body.style.backgroundColor = 'red';
+    document.getElementById("navigation").style.left = "-30em";
 }
 
 makeRed.addEventListener('click', redBackground);
 
 const orangeBackground = () => {
-    background.classList.add('orange');
-    background.classList.remove('red', 'purple', 'green');
+    document.body.style.backgroundColor = 'orange';
+    document.getElementById("navigation").style.left = "-30em";
 }
 
 makeOrange.addEventListener('click', orangeBackground);
 
 const purpleBackground = () => {
-    background.classList.add('purple');
-    background.classList.remove('red', 'orange', 'green');
+    document.body.style.backgroundColor = 'purple';
+    document.getElementById("navigation").style.left = "-30em";
 }
 
 makePurple.addEventListener('click', purpleBackground);
 
 const greenBackground = () => {
-    background.classList.add('green');
-    background.classList.remove('red', 'purple', 'orange');
+    document.body.style.backgroundColor = 'green';
+    document.getElementById("navigation").style.left = "-30em";
 }
 
 makeGreen.addEventListener('click', greenBackground);
